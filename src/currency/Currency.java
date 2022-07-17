db55bea62e05ffe616d87ea873f237332bea7eee
KateDrebot <73437573+KateDrebot@users.noreply.github.com>
2022-07-17 14:37:37 -0700

    project Currency without class Bitcoin()

---
+++ src/currency/Currency.java
@@ -0,0 +1,60 @@
+package currency;
+
+abstract class Currency {
+    private double kursNBU;
+    private double marga = 1.1;
+
+
+    public Currency(double kursNBU) {
+        this.kursNBU = kursNBU;
+
+    }
+
+    public double getKursNBU() {
+        return kursNBU;
+    }
+
+    public void setKursNBU(double kursNBU) {
+
+        this.kursNBU = kursNBU;
+    }
+
+    public double getMarga() {
+
+        return marga;
+    }
+
+    public void setMarga(double marga) {
+
+        this.marga = marga;
+    }
+
+    private double getKursBuy() {
+
+        return kursNBU * marga;
+    }
+
+    private double getKursSell(){
+
+        return kursNBU / marga;
+    }
+
+    abstract String getCurrencyName();
+
+    public double exchangeGrnToCurrency(double numberOfGrn){
+        Double tempResult = numberOfGrn/getKursBuy();
+        //System.out.println("for "+numberOfGrn +"grn you get "+tempResult);
+        System.out.println(String.format("for %s grn you get %s %s by kurs %s (marga %s)"
+                                   ,numberOfGrn,tempResult, getCurrencyName(),getKursBuy(),marga));
+        return tempResult;
+    }
+
+
+    public double  exchangeCurrencyToGrn(double numberOfCurrency){
+        double tempResult = numberOfCurrency *getKursSell();
+        System.out.println(String.format("for %s %s you get %s grn by kurs %s (marga %s)"
+                ,numberOfCurrency,getCurrencyName(),tempResult,getKursSell(),marga));
+        return tempResult;
+    }
+
+}
---
+++ src/currency/MainClass.java
@@ -0,0 +1,23 @@
+package currency;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        Dollar dollar=new Dollar(36);
+        System.out.println("Dollar "+dollar.getKursNBU());
+        dollar.setKursNBU(36.5);
+        System.out.println("Dollar NEW "+dollar.getKursNBU());
+
+        dollar.exchangeGrnToCurrency(4015);
+        dollar.exchangeCurrencyToGrn(1000);
+        dollar.setMarga(1.15);
+        dollar.exchangeGrnToCurrency(4015);
+        dollar.exchangeCurrencyToGrn(1000);
+
+        Euro euro=new Euro(38);
+        euro.exchangeCurrencyToGrn(1000);
+
+
+
+    }
+}
